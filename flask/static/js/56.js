9fd1843b8fd386c8af8c0162a19d9ff04390a482
Nidhin Urmese <nurmese@vmware.com>
2020-07-10 20:13:34 +0530

    Fix kunugu UI

--- flask/static/js/56.js
+++ flask/static/js/56.js
@@ -49,55 +49,33 @@ function refreshHand(data) {
         showVili(data.VSF);
     }
 
-    if(data.names){
-        if (data.KunuguSeat){
-
-
-              populateNames(kunuguLogic(data.names,data.KunuguSeat));
-
-        }
-        else{
-
+    if (data.names) {
+        if (data.KunuguSeat) {
+            populateNames(data.names, data.KunuguSeat);
+        } else {
             populateNames(data.names);
         }
-
     }  // end of name check
 
-
 }
 
-function kunuguLogic(myNames,KunuguSeat){
-    //console.log(KunuguSeat);
-
-                              if (typeof KunuguSeat ===  "undefined")
-                                     return myNames;
-                for (var i = 0; i <KunuguSeat.length; i++) {
-      //            console.log("____________");
-                            if (KunuguSeat[i] == 1)
-                                    myNames["SN1"] = myNames["SN1"] +"M"; // Need to replace with actual kunugu
-                            else if     (KunuguSeat[i] == 2)
-                                    myNames["SN2"] = myNames["SN2"] +"__";
-                            else if     (KunuguSeat[i] == 3)
-                                    myNames["SN3"] = myNames["SN3"] +"__";
-                            else if     (KunuguSeat[i] == 4)
-                                    myNames["SN4"] = myNames["SN4"] +"__";
-                            else if     (KunuguSeat[i] == 5)
-                                    myNames["SN5"] = myNames["SN5"] +"__";
-                            else if     (KunuguSeat[i] == 6)
-                                   myNames["SN6"] = myNames["SN6"] +"__";
-                }
-              //  console.log(myNames);
-                    return (myNames);
-}
-
-
-function populateNames(names) {
-    $('span[name="N1"]')[0].innerHTML = names["SN1"];
-    $('span[name="N2"]')[0].innerHTML = names["SN2"];
-    $('span[name="N3"]')[0].innerHTML = names["SN3"];
-    $('span[name="N4"]')[0].innerHTML = names["SN4"];
-    $('span[name="N5"]')[0].innerHTML = names["SN5"];
-    $('span[name="N6"]')[0].innerHTML = names["SN6"];
+function populateNames(names, kunuguSeat) {
+    //if there is no kunuguSeat
+    if (typeof kunuguSeat === "undefined") {
+        for(let i=1; i<=6; i++) {
+            let tempSpan = "span[name=\"N"+ i + "\"]"
+            $(tempSpan)[0].innerHTML = names["SN" + i];
+        }
+    } else { // add kunugu icon next to name.
+        for(let i=1; i<=6; i++) {
+            let tempSpan = "span[name=\"N"+ i + "\"]"
+            if(kunuguSeat.includes(i)) {
+                $(tempSpan)[0].innerHTML = names["SN" + i] + "🤡";
+            } else {
+                $(tempSpan)[0].innerHTML = names["SN" + i];
+            }
+        }
+    }
     // backgorund color
      $('span[name="N1"]')[0].style.backgroundColor="black";
      $('span[name="N2"]')[0].style.backgroundColor="red";
@@ -106,9 +84,8 @@ function populateNames(names) {
      $('span[name="N5"]')[0].style.backgroundColor="black";
      $('span[name="N6"]')[0].style.backgroundColor="red";
      globalData.names=names;
-
-
 }
+
 function showVili(VSF) {
     if (VSF) {
         resetVSF();
@@ -389,8 +366,8 @@ function goToSeat(roomNo, seatNo) {
                 $('div[id="foldSection"]').show();
                       //myNames=globalData.names;
                       console.log(globalData.names);
-                      myNames=kunuguLogic(globalData.names,data.KunuguSeat);
-                      populateNames(myNames);
+                      //myNames=kunuguLogic(globalData.names,data.KunuguSeat);
+                      populateNames(myNames, data.KunuguSeat);
 
             }
 
@@ -436,7 +413,7 @@ function goToSeat(roomNo, seatNo) {
                 if (data.Villi != "P") {
                     //console.log(data.dude);
                   //  console.log(data.dudeTeam);
-                    $("#trumpSection")[0].innerHTML = "Bidding : " + convertToSign(data.Villi.substring(0, 1)) + data.Villi.substring(1) + "  by "+data.dude;
+                    $("#trumpSection")[0].innerHTML = "Bid : " + convertToSign(data.Villi.substring(0, 1)) + data.Villi.substring(1) + "  by "+data.dude;
 
                     if (data.dudeTeam == 'Team1')
                     {
